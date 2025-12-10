0327dd0bc249152887ae7f9e56a212563a8dede9
omundifelix <omundifelix30@gmail.com>
2025-12-11 00:58:04 +0300

    ngrok updates

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ const ngrok = require('ngrok');
 const app = express();
 require('dotenv').config()
 const PORT = 3000;
-// const cors = require("cors");
 
- 
-app.use(express.json());  
-// app.use(cors({ origin: ['127.0.0.1', '2394-41-209-51-2.ngrok.io'] }));
+app.use(express.json());
 
 async function Connect() {
     const url = await ngrok.connect({
         proto: 'http',
-        addr: 3000,
-        authtoken:process.env.AUTHTOKEN,
+        addr: 8049,
+        authtoken: process.env.NGROK_AUTHTOKEN,
     });
     console.log(url)
 };
@@ -23,13 +20,13 @@ Connect();
 app.use("/", async (req, res) => {
     res.send("Welcome Home")
 });
- 
-app.listen(PORT, (error) =>{
-if (!error) {
-console.log(`Server is Successfully Running on port ${PORT}`)
-}
-else {
-console.log("Error occurred", error);
-}
+
+app.listen(PORT, (error) => {
+    if (!error) {
+        console.log(`Server is Successfully Running on port ${PORT}`)
+    }
+    else {
+        console.log("Error occurred", error);
+    }
 }
 );
